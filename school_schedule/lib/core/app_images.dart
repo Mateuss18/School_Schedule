b090658d315c7732e807cf55b146a2bb5eab2261
Mateuss18 <mateusdfl18@gmail.com>
2022-03-27 10:15:27 -0300

    correct directory

--- school_schedule/lib/core/app_images.dart
+++ school_schedule/lib/core/app_images.dart
@@ -3,8 +3,7 @@ import 'package:flutter_svg/svg.dart';
 
 class AppImages {
   final Widget svg = SvgPicture.asset(
-  'assets/images/agenda.svg',
-  semanticsLabel: 'icone agenda'
-  
+  'assets/images/calendario-login.svg',
+  semanticsLabel: 'calendario login'
 );
 }
